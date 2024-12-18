99af45da42ebd64308e5f1ad4019f1fdbc6a4cac
justamitsaha <justamitsaha@gmail.com>
2024-12-18 20:01:12 +0530

    Clearing of unwanted repository classes

--- springBootJPA/src/main/java/com/saha/amit/service/CustomerService.java
+++ springBootJPA/src/main/java/com/saha/amit/service/CustomerService.java
@@ -1,15 +1,9 @@
 package com.saha.amit.service;
 
-import com.saha.amit.dto.CustomerDto;
-import com.saha.amit.dto.ProfileDto;
 import com.saha.amit.model.Customer;
-import com.saha.amit.model.Profile;
 import com.saha.amit.repository.CustomerRepository;
-import com.saha.amit.repository.ProfileRepository;
-import org.apache.catalina.mapper.Mapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +15,6 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
-    @Autowired
-    ProfileRepository profileRepository;
 
     private final Log log = LogFactory.getLog(CustomerService.class);
 
