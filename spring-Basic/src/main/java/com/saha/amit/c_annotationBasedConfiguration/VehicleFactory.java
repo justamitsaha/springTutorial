6244b96516f1074f7011a6c520582c61fe115f9a
472842 <amit87.s@tcs.com>
2025-07-18 17:58:41 +0530

    doc update

--- spring-Basic/src/main/java/com/saha/amit/c_annotationBasedConfiguration/VehicleFactory.java
+++ spring-Basic/src/main/java/com/saha/amit/c_annotationBasedConfiguration/VehicleFactory.java
@@ -9,6 +9,11 @@ public class VehicleFactory {
     @Autowired
     private ApplicationContext context;
 
+    /**
+     * @param type name of the bean
+     * @return Vehicle type Car/Bike based on the input
+     * Based on the input param car/bike respective Class will be returned
+     */
     public Vehicle getVehicle(String type) {
         return context.getBean(type, Vehicle.class);
     }
