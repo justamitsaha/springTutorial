b919337fb131e5f5118bfa93f605c76471a3f1f1
Amit Saha <justamitsaha@gmail.com>
2025-08-03 07:46:42 +0530

    Create product and order fix

--- springJdbcTemplate/src/main/java/com/saha/amit/controller/CreateQueryController.java
+++ springJdbcTemplate/src/main/java/com/saha/amit/controller/CreateQueryController.java
@@ -3,8 +3,11 @@ package com.saha.amit.controller;
 import com.saha.amit.AppConstants;
 import com.saha.amit.dto.CustomerDto;
 import com.saha.amit.dto.OrderDto;
+import com.saha.amit.dto.ProductDto;
 import com.saha.amit.repository.CreateQueryRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,8 @@ import java.util.List;
 @RequestMapping("create")
 public class CreateQueryController {
 
+    Logger logger = LoggerFactory.getLogger(CreateQueryController.class);
+
     private final CreateQueryRepository query;
 
     @Autowired
@@ -24,22 +29,25 @@ public class CreateQueryController {
 
 
     @Operation(summary = "Does LIKE operation on customer name and email ")
-    @PutMapping("1/customer")
+    @PutMapping("customer")
     public ResponseEntity<Long> insertCustomer(@RequestBody CustomerDto customerDto){
         return ResponseEntity.ok().body(query.insertCustomer(customerDto));
     }
 
-    @Operation(summary = "Does LIKE operation on customer name and email ")
-    @PutMapping("2/product/{productName}/{price}")
-    public ResponseEntity<Long> addProductWithCategories(@PathVariable String productName,
-                                                         @PathVariable double price,
-                                                         @RequestBody List<Long> categoryIds){
-        return ResponseEntity.ok().body(query.addProductWithCategories(productName, price, categoryIds));
+    @Operation(summary = "Add product & categories", description = AppConstants.CREATE_PRODUCT_DESCRIPTION
+    )
+    @PutMapping("product")
+    public ResponseEntity<Long> addProductWithCategories(@RequestBody ProductDto productDto){
+        logger.info("Add product productDto: {}", productDto);
+        return ResponseEntity.ok().body(query.addProductWithCategories(productDto.getName(),
+                productDto.getPrice(),
+                productDto.getCategoryIds()));
     }
 
-    @Operation(summary = AppConstants.CREATE_ORDER_DESCRIPTION)
-    @PutMapping("3/order/{customerId}")
-    public ResponseEntity<Integer> createOrders(@PathVariable long customerId, @RequestBody OrderDto orderDto){
+    @Operation(summary = "Create order, Payments", description = AppConstants.CREATE_ORDER_DESCRIPTION)
+    @PutMapping("order/{customerId}")
+    public ResponseEntity<String> createOrders(@PathVariable long customerId, @RequestBody OrderDto orderDto){
+        logger.info("createOrders order data: {} , customer id: {}", orderDto.toString(), customerId);
         return ResponseEntity.ok().body(query.createOrders(orderDto,customerId));
     }
 
--- springJdbcTemplate/src/main/java/com/saha/amit/dto/OrderDto.java
+++ springJdbcTemplate/src/main/java/com/saha/amit/dto/OrderDto.java
@@ -2,14 +2,11 @@ package com.saha.amit.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
-import java.util.Map;
 
+@ToString
 @Getter
 @Setter
 @AllArgsConstructor
@@ -19,6 +16,13 @@ public class OrderDto {
     private String orderUuid;
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String orderNumber;
+    // Since productUuid in ProductDto can't be updated due to READ_ONLY
+    // hence adding List of productIds which will be sent from swagger/client
+    @Schema(
+            description = "List of product IDs",
+            example = "[1, 2, 3]"
+    )
+    List<Integer> productIds;
     List<ProductDto> products;
     private PaymentStatus paymentStatus;
 }
